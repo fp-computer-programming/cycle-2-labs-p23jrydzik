f1b50d7265523bfa1ab95d003067030fed29e9cb
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-09-15 10:37:32 -0400

    Finished labs 1 and 3 so far

---
+++ lab_2-3.py
@@ -0,0 +1,15 @@
+# author: JHR 9/15/22
+
+a = 2
+print (type (a))
+
+a = float(a)
+print (type (a))
+
+a = str(a)
+print (type (a))
+
+a = bool(a)
+print (a == 2)
+
+# It came out as false because it is no longer an integer and is now a string
