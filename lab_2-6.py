6931466346db3f4ef0f139807ed370baff622bb7
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-09-20 13:32:39 -0400

    finished labs 6 and 7

---
+++ lab_2-6.py
@@ -0,0 +1,26 @@
+# author JHR 9/20/22
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print ("a is an even number")
+else:
+    print ("a is an odd number")
+
+if b % 2 == 0:
+    print ("b is an even number")
+else:
+    print ("a is an odd number")
+
+if c % 2 == 0:
+    print ("c is an even number")
+else:
+    print ("a is an odd number")
+
+if d % 2 == 0:
+    print ("d is an even number")
+else:
+    print ("a is an odd number")
---
+++ lab_2-7.py
@@ -0,0 +1,54 @@
+# author JHR 9/20/22
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print ("a is an even number")
+else:
+    print ("a is an odd number")
+
+if a < -5:
+    print ("a is less than negative 5")
+elif -5 < a < 5:
+    print ("a is between negative 5 and positive 5")
+else:
+    print ("a is greater than 5")
+
+if b % 2 == 0:
+    print ("b is an even number")
+else:
+    print ("a is an odd number")
+
+if b < -5:
+    print ("b is less than negative 5")
+elif -5 < b < 5:
+    print ("b is between negative 5 and positive 5")
+else:
+    print ("b is greater than 5")
+
+if c % 2 == 0:
+    print ("c is an even number")
+else:
+    print ("a is an odd number")
+
+if c < -5:
+    print ("c is less than negative 5")
+elif -5 < c < 5:
+    print ("c is between negative 5 and positive 5")
+else:
+    print ("c is greater than 5")
+
+if d % 2 == 0:
+    print ("d is an even number")
+else:
+    print ("a is an odd number")
+
+if d < -5:
+    print ("d is less than negative 5")
+elif -5 < d < 5:
+    print ("d is between negative 5 and positive 5")
+else:
+    print ("d is greater than 5")
